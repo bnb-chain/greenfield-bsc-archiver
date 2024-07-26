c79fc32a34b8df75daa097527318951b3f59ecad
BarryTong65 <barrytong.work@gmail.com>
2024-07-26 14:58:07 +0800

    fix: tiny fix

--- db/block.go
+++ db/block.go
@@ -12,7 +12,7 @@ const (
 
 type Block struct {
 	Id          int64
-	BlockHash   common.Hash `gorm:"NOT NULL;type:BINARY(32);index:idx_block_hash;"`
+	BlockHash   common.Hash `gorm:"NOT NULL;type:BINARY(32);index:idx_block_hash,prefix_len:32"`
 	BlockNumber uint64      `gorm:"NOT NULL;uniqueIndex:idx_block_number;index:idx_block_number_status,priority:2"`
 
 	BundleName string `gorm:"NOT NULL"`

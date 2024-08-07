388d77cc4c3c3f3dd54802a030dcf8d4fe79fe65
BarryTong65 <barrytong.work@gmail.com>
2024-08-07 11:29:26 +0800

    fix: create temp

--- cmd/greeenfield-bsc-archiver-syncer/main.go
+++ cmd/greeenfield-bsc-archiver-syncer/main.go
@@ -43,6 +43,10 @@ func main() {
 	db := config.InitDBWithConfig(&cfg.DBConfig, true)
 	blockDB := syncerdb.NewBlockSvcDB(db)
 	bs := syncer.NewBlockIndexer(blockDB, cfg)
+	err := os.MkdirAll(cfg.TempDir, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 	go bs.StartLoop()
 
 	if cfg.MetricsConfig.Enable {
